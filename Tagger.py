62056e2bd312f8335f6e981b39c346fe716083b4
Wim Zaagman <wim.zaagman@wastevision.com>
2023-12-24 15:55:53 +0100

    adding sockets

--- Tagger.py
+++ Tagger.py
@@ -1,6 +1,8 @@
 """
 
 """
+import queue
+import socket
 
 # Import the required modules
 import nicegui as ng
@@ -16,6 +18,7 @@ test_data = {
     'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
 }
 
+rx_queue = queue.Queue()
 gun_info = pd.DataFrame(test_data)
 
 table = ui.table(
@@ -26,20 +29,32 @@ table = ui.table(
 age = 103
 
 
-def add_row():
-    global table, age, gun_info
+def rx_UDP():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(('0.0.0.0', 15677))  # Listen on all available interfaces
     while True:
-        new_row = {'Name': 'Eve', 'Age': age, 'City': 'Miami'}
-        age += 1
-        gun_info = pd.concat([gun_info, pd.DataFrame([new_row])], ignore_index=True)
-        table.update_rows(gun_info.to_dict('records'))
-        time.sleep(0.5)  # Sleep for 5 seconds before adding another row
+        data, addr = udp_socket.recvfrom(1024)  # blocking wait for packet
+        rx_queue.put(data.decode('utf-8', errors='ignore'))
+        ip, port = addr
 
+def process_UDP():
+    global age, gun_info
+    while True:
+        while not rx_queue.empty():
+            new_row = {'Name': 'Eve', 'Age': age, 'City': 'Miami'}
+            age += 1
+            gun_info = pd.concat([gun_info, pd.DataFrame([new_row])], ignore_index=True)
+            table.update_rows(gun_info.to_dict('records'))
+        time.sleep(0.1)  # Sleep for 100ms
 
-# Start the thread
-update_thread = threading.Thread(target=add_row)
-update_thread.daemon = True
-update_thread.start()
+# Start the threads
+UDP_RX = threading.Thread(target=rx_UDP)
+UDP_RX.daemon = True
+UDP_RX.start()
+
+packet_proces = threading.Thread(target=process_UDP)
+packet_proces.daemon = True
+packet_proces.start()
 
 # Display the DataFrame using NiceGUI
 ui.run(port=9000)
---
+++ UDP_tools.py
@@ -0,0 +1,21 @@
+import socket
+
+
+def UDP_broadcast(message, port):
+    # Create a UDP socket for broadcasting
+    broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    broadcast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Set the broadcast address
+    broadcast_address = ('<broadcast>', port)  # all computers in this segment. LAN
+    # broadcast_address = ('127.0.0.1', port)  # only my pc.
+
+    try:
+        # Send the broadcast message
+        broadcast_socket.sendto(message.encode(), broadcast_address)
+        # print(f"Broadcasted message to {broadcast_address}: {message}")
+    except Exception as e:
+        print(f"Error sending broadcast message: {str(e)}")
+    finally:
+        # Close the socket
+        broadcast_socket.close()
